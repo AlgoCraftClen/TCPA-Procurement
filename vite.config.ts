e171bff2cfd89b294876d3097d7aa427d03bd7ed
Clenny Minor <clennyminor@gmail.com>
2025-05-26 11:18:23 +1200

    Launch check

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/Form-Flow/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
